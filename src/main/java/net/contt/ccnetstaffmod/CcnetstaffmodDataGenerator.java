8712de346fd9ea9e16079411b665d4ada703a11d
Cont <contdj8389@gmail.com>
2025-03-06 01:34:30 +0530

    Initial commit

---
+++ src/main/java/net/contt/ccnetstaffmod/CcnetstaffmodDataGenerator.java
@@ -0,0 +1,11 @@
+package net.contt.ccnetstaffmod;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class CcnetstaffmodDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
