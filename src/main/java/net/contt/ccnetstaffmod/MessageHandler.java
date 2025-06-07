d741c9660558f2f29137e72720af114bf9b21166
Perpltxed <40055823+M8E-ya@users.noreply.github.com>
2025-06-07 15:33:56 +0530

    Update MessageHandler.java

--- src/main/java/net/contt/ccnetstaffmod/MessageHandler.java
+++ src/main/java/net/contt/ccnetstaffmod/MessageHandler.java
@@ -11,18 +11,37 @@ public class MessageHandler {
 
     public static boolean handleMessage(Text message, boolean overlay) {
         String messageContent = message.getString();
+        boolean isSilentMessage = messageContent.startsWith("[Silent]");
         boolean isStaffMessage = (messageContent.startsWith("(A)") && messageContent.contains("→")) ||
                 (messageContent.matches("^\\[[^\\]]+\\] .*».*") && !messageContent.startsWith("[Discord]")) ||
-                messageContent.startsWith("[Silent]");
+                isSilentMessage;
 
         if (concealMessages && isStaffMessage) {
-            Text hoverText = Text.literal(messageContent).setStyle(Style.EMPTY.withColor(Formatting.AQUA));
-            Text notification = Text.literal("CC")
-                    .setStyle(Style.EMPTY.withColor(Formatting.WHITE).withBold(true))
-                    .append(Text.literal("Net ").setStyle(Style.EMPTY.withColor(Formatting.GREEN).withBold(true)))
-                    .append(Text.literal("» ").setStyle(Style.EMPTY.withColor(Formatting.DARK_GRAY).withBold(false)))
-                    .append(Text.literal("Staff Message").setStyle(Style.EMPTY.withColor(Formatting.GREEN).withBold(false)))
-                    .append(Text.literal(" [Hover to see]").setStyle(Style.EMPTY.withColor(Formatting.AQUA).withBold(false).withHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, hoverText))));
+            Text hoverText = Text.literal(messageContent)
+                    .setStyle(Style.EMPTY.withColor(Formatting.AQUA));
+
+            Text notification;
+
+            if (isSilentMessage) {
+                notification = Text.literal("CC")
+                        .setStyle(Style.EMPTY.withColor(Formatting.WHITE).withBold(true))
+                        .append(Text.literal("Net ").setStyle(Style.EMPTY.withColor(Formatting.RED).withBold(true)))
+                        .append(Text.literal("» ").setStyle(Style.EMPTY.withColor(Formatting.DARK_GRAY).withBold(false)))
+                        .append(Text.literal("Silent Action").setStyle(Style.EMPTY.withColor(Formatting.RED).withBold(false)))
+                        .append(Text.literal(" [Hover to see]").setStyle(
+                                Style.EMPTY.withColor(Formatting.AQUA).withBold(false).withHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, hoverText))
+                        ));
+            } else {
+                notification = Text.literal("CC")
+                        .setStyle(Style.EMPTY.withColor(Formatting.WHITE).withBold(true))
+                        .append(Text.literal("Net ").setStyle(Style.EMPTY.withColor(Formatting.GREEN).withBold(true)))
+                        .append(Text.literal("» ").setStyle(Style.EMPTY.withColor(Formatting.DARK_GRAY).withBold(false)))
+                        .append(Text.literal("Staff Message").setStyle(Style.EMPTY.withColor(Formatting.GREEN).withBold(false)))
+                        .append(Text.literal(" [Hover to see]").setStyle(
+                                Style.EMPTY.withColor(Formatting.AQUA).withBold(false).withHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, hoverText))
+                        ));
+            }
+
             MinecraftClient.getInstance().inGameHud.getChatHud().addMessage(notification);
             return false;
         }
