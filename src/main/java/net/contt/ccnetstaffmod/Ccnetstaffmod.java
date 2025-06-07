b7cc41166eb957ad0b8d4ea7fa70d17917e41b18
Perpltxed <40055823+M8E-ya@users.noreply.github.com>
2025-06-07 12:09:41 +0530

    Update Ccnetstaffmod.java

--- src/main/java/net/contt/ccnetstaffmod/Ccnetstaffmod.java
+++ src/main/java/net/contt/ccnetstaffmod/Ccnetstaffmod.java
@@ -1,30 +1,20 @@
 package net.contt.ccnetstaffmod;
 
-import net.fabricmc.api.ModInitializer;
 import net.fabricmc.api.ClientModInitializer;
-import net.minecraft.client.MinecraftClient;
-import net.minecraft.text.Text;
-import net.minecraft.text.HoverEvent;
-import net.minecraft.text.Style;
 import net.fabricmc.fabric.api.client.message.v1.ClientReceiveMessageEvents;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class Ccnetstaffmod implements ClientModInitializer {
-	public static final String MOD_ID = "ccnetstaffmod";
-	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
-	@Override
-	public void onInitializeClient() {
-		ClientReceiveMessageEvents.ALLOW_CHAT.register((message, signedMessage, sender, params, callback) -> {
-			String messageContent = message.getString();
+    public static final String MOD_ID = "ccnetstaffmod";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
-			if (messageContent.startsWith("[Silent]") || messageContent.startsWith("[SC]")) {
-				Text hoverText = Text.literal(messageContent);
-				Text notification = Text.literal("CCNet Staff Mod » New Notification [Hover to see]")
-						.setStyle(Style.EMPTY.withHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, hoverText)));
+    @Override
+    public void onInitializeClient() {
+        ClientReceiveMessageEvents.ALLOW_GAME.register(MessageHandler::handleMessage);
 
-			}
-			return true;
-		});
-	}
+        CommandHandler.registerCommands();
+
+        LOGGER.info("CCNet Staff Mod initialized successfully!");
+    }
 }
